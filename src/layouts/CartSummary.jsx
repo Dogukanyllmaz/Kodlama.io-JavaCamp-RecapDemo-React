adce1e9e6ea64ac5f0b7d30ae405d07a6ef1221f
Hüseyin Doğukan Yılmaz <dogu_codeM@hotmail.com>
2021-06-17 00:19:31 +0300

    Add product to cart method

--- src/layouts/CartSummary.jsx
+++ src/layouts/CartSummary.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import { Dropdown, DropdownDivider, DropdownItem } from "semantic-ui-react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  Dropdown,
+  DropdownDivider,
+  DropdownItem,
+  Label,
+} from "semantic-ui-react";
+import { useSelector } from "react-redux";
 
 export default function CartSummary() {
-    return (
-        <div>
-            <Dropdown item text="Sepetim">
-              <Dropdown.Menu>
-                <Dropdown.Item>product</Dropdown.Item>
-                <Dropdown.Item>product</Dropdown.Item>
-                <Dropdown.Item>product</Dropdown.Item>
-                <Dropdown.Divider/>
-                <Dropdown.Item as={NavLink} to="/cart-details" >Sepete Git</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-        </div>
-    )
+  const { cartItems } = useSelector((state) => state.cart);
+
+  return (
+    <div>
+      <Dropdown item text="Sepetim">
+        <Dropdown.Menu>
+          {cartItems.map((cartItem) => (
+            <Dropdown.Item key={cartItem.product.id}>
+              {cartItem.product.productName}
+              <Label>{cartItem.quantity}</Label>
+            </Dropdown.Item>
+          ))}
+
+          <Dropdown.Divider />
+          <Dropdown.Item as={NavLink} to="/cart-details">
+            Sepete Git
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 }
